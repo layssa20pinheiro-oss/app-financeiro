076520abc4c2d8cfdf4450457c015bbae523ffe0
layssa20pinheiro-oss <layssa20pinheiro@gmail.com>
2026-09-01 10:32:53 -0300

    Replace Supabase anon key with a new value
    Updated the Supabase anonymous key with a new value.

--- supabase.js
+++ supabase.js
@@ -2,8 +2,11 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
+// URL base do seu projeto Supabase
 const supabaseUrl = 'https://cbhdlewbvegrsdbrdszw.supabase.co';
-const supabaseAnonKey = 'SUA_PUBLISHABLE_KEY_AQUI'; 
+
+// Sua Publishable Key copiada do painel
+const supabaseAnonKey = 'sb_publishable_1cRh5f00jopLbhnapQNaSw_RZMxO'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
